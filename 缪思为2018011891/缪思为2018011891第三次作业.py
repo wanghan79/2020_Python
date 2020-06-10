3477db42b660b99ecfed777c7e153e8225243ea7
MuseWEI <63635304+MuseWEI@users.noreply.github.com>
2020-06-10 10:19:23 +0800

    第二次第三次作业提交

---
+++ 缪思为2018011891/缪思为2018011891第三次作业.py
@@ -0,0 +1,18 @@
+class PrimeNumbers:
+    def __init__(self,start,end):
+        self.start=start
+        self.end=end
+    def isPrimeNum(self,k):
+        if k<2:
+            return False
+        for i in range(2,k):
+            if k%i==0:
+                return False
+        return True
+    def __iter__(self):
+        for k in range(self.start,self.end):
+            if self.isPrimeNum(k):
+                yield k
+
+for i in PrimeNumbers(1,1000):
+    print(i)
