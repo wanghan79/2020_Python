7cd69d6e03695b9060e79b565d4827df1604f092
wtc202 <63712867+wtc202@users.noreply.github.com>
2020-06-23 16:15:58 +0800

    Update 平时作业二.py

--- 王添晨 2018013132/平时作业二.py
+++ 王添晨 2018013132/平时作业二.py
@@ -3,22 +3,23 @@ import string
 
 
 def dataSampling(func):
+    
     def wrapper(datatype,datarange,num,*args,strlen=8):
         result=set()
         if datatype is int:
             while len(result)<num:
-                it=iter(datarange)
-                item=random.randint(next(it),next(it))
-                result.add(item)
+            it=iter(datarange) item=random.randint(next(it),next(it))
+            result.add(item)
+            
         elif datatype is float:
             while len(result)<num:
-                it=iter(datarange)
-                item=random.uniform(next(it),next(it))
-                result.add(item)
+            it=iter(datarange) item=random.uniform(next(it),next(it))
+            result.add(item)
+            
         elif datatype is str:
             while len(result)<num:
-                item=''.join(random.SystemRandom().choice(datarange) for _ in range(strlen))
-                result.add(item)
+            item=''.join(random.SystemRandom().choice(datarange) for _ in range(strlen))
+            result.add(item)
         return func(result,*args)
 
 
@@ -26,18 +27,13 @@ def dataSampling(func):
 
 
 
-@DataSampling
 def dataScreening(data,*args):
         result = set()
         for item in data:
-            if type(item) is int or type(item) is float:
+            if type(item) is int or type(item) is float: 
                 it = iter(args)
-                if next(it)<=item and next(it)>=item:
-                    result.add(item)
-            elif type(item) is float:
-                it = iter(args)
-                if next(it)<=item and next(it)>=item:
-                    result.add(item)
+            if next(it) <= item <= next(it): 
+                result.add(item)
             elif type(item) is str:
                 for subster in args:
                     if subster in item:
