252c06a9530660b1407cc15d27df1ed90e5b5f50
wtc202 <63712867+wtc202@users.noreply.github.com>
2020-06-23 16:05:07 +0800

    Update 平时作业一.py

--- 王添晨 2018013132/平时作业一.py
+++ 王添晨 2018013132/平时作业一.py
@@ -7,14 +7,11 @@ def dataSampling(datatype, datarange, num, strlen=8):
         result = set()
         if datatype is int:
             while len(result) < num:
-                it = iter(datarange)
-                item = random.randint(next(it), next(it))
+                it = iter(datarange) item = random.randint(next(it), next(it))
                 result.add(item)
-
         elif datatype is float:
             while len(result) < num:
-                it = iter(datarange)
-                item = random.uniform(next(it), next(it))
+                it = iter(datarange) item = random.uniform(next(it), next(it))
                 result.add(item)
         elif datatype is str:
             while len(result) < num:
@@ -22,13 +19,10 @@ def dataSampling(datatype, datarange, num, strlen=8):
                 result.add(item)
 
 
-
-    except TypeError:
-        print("类型错误，请输入正确类型的数")
-    except ValueError:
-        print("参数错误，请输入正确的数")
-    except NameError:
-        print("请先输入数据类型")
+    except TypeError: print("Wrong type, please enter the correct type of data")
+    except ValueError: print("Please enter the correct data")
+    except NameError: print("Please enter the data type at first")
+        
     else:
         return result
     finally:
@@ -41,43 +35,46 @@ def dataScreening(data, *term):
         for item in data:
             if type(item) is int or type(item) is float:
                 it = iter(term)
-                if next(it) <= item and next(it) >= item:
+                if next(it) <= item <=next(it):
                     result.add(item)
             elif type(item) is float:
                 it = iter(term)
-                if next(it) <= item and next(it) >= item:
+                if next(it) <= item <= next(it):
                     result.add(item)
             elif type(item) is str:
                 for Screening_str in term:
                     if Screening_str in item:
                         result.add(item)
-    except TypeError:
-        print("类型错误，请输入正确类型的数")
-    except ValueError:
-        print("参数错误，请输入正确的数")
-    except NameError:
-        print("请先输入数据类型")
+                        
+                        
+    except TypeError: print("Wrong type, please enter the correct type of data")
+    except ValueError: print("Please enter the correct data")
+    except NameError: print("Please enter the data type at first")
+        
     else:
         return result
     finally:
-        print("筛选数据：")
+        print("Filter data as follows：")
 
 
 def apply():
 
 
+    #整型
     result_int = dataSampling(int, [0,500], 50)
     print(result_int)
     int_Screening = dataScreening(result_int,10,20)
     print(int_Screening)
 
 
+    #浮点型
     result_float = dataSampling(float, [0, 500], 50)
     print(result_float)
     float_Screening = dataScreening(result_float,10,20)
     print(float_Screening)
 
-
+    
+    #字符型
     result_str = dataSampling(str,string.ascii_letters,20)
     print(result_str)
     str_Screening = dataScreening(result_str,'up')
